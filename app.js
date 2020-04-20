1cd0fe8822439364de96b8ebe62cd43d234aece0
Jjmcclain <31944090+Jjmcclain-Uiowa@users.noreply.github.com>
2020-04-19 19:28:53 -0500

    .

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(express.static('public'))
-app.get('/', (req, res) => res.sendFile('./index.html'))
+app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile('./index.html'));
 
 // app.listen(port, () => {
 //     console.log('Express server listening on port', port)
 //   });
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+	console.log(`Example app listening at http://localhost:${port}`)
+);
