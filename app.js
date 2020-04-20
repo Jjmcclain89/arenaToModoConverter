af996087972124ab7db594a0a76e6366a823d218
Jjmcclain <31944090+Jjmcclain-Uiowa@users.noreply.github.com>
2020-04-19 19:25:49 -0500

    .

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 8080 || process.env.PORT
 
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile('./index.html'))
